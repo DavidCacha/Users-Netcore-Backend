b8bc3517a1e4aab1e6b7e7c3a6917ea13196d9c7
LuisDavidCasanovaChavez <luisdcasanova1995@gmail.com>
2025-02-16 18:11:15 -0600

    Initial commit for Render deployment

--- NetCoreApi/Program.cs
+++ NetCoreApi/Program.cs
@@ -1,13 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Obtener el puerto desde la variable de entorno de Render
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.Urls.Add($"http://*:{port}");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,9 +19,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
